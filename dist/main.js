3134d53cd48bab7c3b98f651675ba1b8b99ad12c
euclidesdry <euclidesdry@gmail.com>
2021-12-23 08:25:35 +0100

    Basic configuration of Webpack :snowflake:
    - Making webpack.config.js file;
    - Set Webpack to read and generate a dundle;
    - Making src/index.js file;
    - Generate the bundle with Webpack

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+console.log("O webpach está funcionando correctamente!");
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const path = require('path');
 
 module.export = {
+    mode: 'none',
     entry: path.join(__dirname, 'src', 'index'),
     output: {
         path: path.join(__dirname, 'dist'),
